af1b4c668514eae77212f693c0e1c9c999ec0124
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2022-07-27 20:53:24 +0530

    Update checkout_git.groovy

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,4 +1,4 @@
-def checkout_git(String repo) {
+def checkout_git() {
     
     echo "This is for checking out git code."
    git credentialsId: 'krishnavamshi933', url: 'git@github.com:krishnavamshi933/java-hello-world-with-maven.git'    
